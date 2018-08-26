eb361c5d91b36bb4a2cee6adcc15614afc7c29f2
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-08-26 17:42:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(items, item) {
+    return items.filter(function(e){
+        return e.toLowerCase() === item.toLowerCase();
+    })
+}
+
+function fuzzyMatch(items, letters) {
+    return items.filter(function(e){
+        return e.includes(letters) && e.indexOf(letters) === 0;
+    })
+}
+
+function matchName(items, name) {
+    return items.filter(function(e){
+        return e.name === name
+    })
+}
